bdaed0e491c76c2e620795cd9678d91eb93eeccd
faizan542 <faizansilat100@gmail.com>
2022-10-10 18:56:38 +0500

    Created Digital Clock

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import './App.css';
 function App() {
   const currentTime = new Date().toLocaleTimeString();
   const [time, setTime] =useState(currentTime);
+  const [getTime, setGetTime] =useState(currentTime);
 
   function updateTime(){
     const newTime = new Date().toLocaleTimeString();
     setTime(newTime);
   }
+
+  function getUpdatedTime(){
+    const getNewTime = new Date().toLocaleTimeString();
+    setGetTime(getNewTime)
+  }
 setInterval(updateTime,1000) //If I remove this set interval than I use Get Time button to get current time
   return (
     <div className="App">
       <h1>{time}</h1>
-      <button onClick={updateTime}>Get Time</button>
+      <button onClick={getUpdatedTime}>Get Time</button>
+      <h1>{getTime}</h1>
     </div>
   );
 }
